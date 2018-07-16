e74a3ab2111c1161abd8027241f50314852041b8
Amo Moloko <u14284783@tuks.co.za>
2018-07-16 16:47:20 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,43 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
-
-function App() {
-  const list = [
-    {
-      title: "React",
-      url: "https://facebook.github.io/react/",
-      author: "Adolf Van Coller",
-      num_comments: 3,
-      points: 4,
-      objectID: 0
-    },
-    {
-      title: "Redux",
-      url: "https://github.com/reactjs/redux",
-      author: "Dan Abramov",
-      num_comments: 4,
-      points: 7,
-      objectID: 1
-    }
-  ];
-
-  return (
-    <div className="App">
-      {list.map(item => (
-        <div key={item.objectID}>
-          <span>
-            <a href="{item.url}">{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>
-      ))};
-    </div>
-  );
-}
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
