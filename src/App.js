d646585627a02f837adfe53d9d6c321424393c41
Amo Moloko <u14284783@tuks.co.za>
2018-07-16 17:08:37 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "./styles.css";
 
+
+
+
 const list = [
   {
     title: "React",
@@ -21,10 +24,22 @@ const list = [
 ];
 
 class App extends Component {
+constructor(props){
+  super(props);
+
+  this.state = {
+    list,
+  };
+}
+
+
+
+
+
   render() {
     return (
       <div className="App">
-        {list.map(item => (
+        {this.state.list.map(item => (
           <div key={item.objectID}>
             <span>
               <a href="{item.url}">{item.title}</a>
@@ -34,6 +49,7 @@ class App extends Component {
             <span>{item.points}</span>
           </div>
         ))};
+        <div> </div>
       </div>
     );
   }
